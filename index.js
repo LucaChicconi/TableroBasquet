09abe5dbe8c277368ad3dd469f98e056da8a9ea2
Luca <lucachicconi997@gmail.com>
2025-11-30 15:26:10 -0300

    dddddd

--- index.js
+++ index.js
@@ -36,4 +36,32 @@ function newGame(){
     guestScore.textContent = 0
 }
 
+document.getElementById("single-home").addEventListener("click", () => updateScore("single-home"))
+document.getElementById("double-home").addEventListener("click", () => updateScore("double-home"))
+document.getElementById("single-guest").addEventListener("click", () => updateScore("single-guest"))
+document.getElementById("double-guest").addEventListener("click", () => updateScore("double-guest"))
+document.getElementById("triple-home").addEventListener("click", () => updateScore("triple-home"))
+document.getElementById("triple-guest").addEventListener("click", () => updateScore("triple-guest"))
+
+function updateScore(id){
+  if(id === "single-home"){
+    homeScore.textContent = parseInt(homeScore.textContent) +1
+  }
+  if(id === "single-guest"){
+    guestScore.textContent = parseInt(guestScore.textContent) +1
+  }
+  if(id === "double-home"){
+    homeScore.textContent = parseInt(homeScore.textContent) +2
+  }
+  if(id === "double-guest"){
+    guestScore.textContent = parseInt(guestScore.textContent) +2
+  }
+  if(id === "triple-home"){
+    homeScore.textContent = parseInt(homeScore.textContent) +3
+  }
+  if(id === "triple-guest"){
+    guestScore.textContent = parseInt(guestScore.textContent) +3
+  }
+}
+
 console.log("Hola Mundo!")
