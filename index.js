5e2b101d749295af1e8fb72c13cc9bd23f72dcbc
Luca <lucachicconi997@gmail.com>
2025-11-30 15:36:07 -0300

    Gracias al Diego por ayudarme con este proyecto!

--- index.js
+++ index.js
@@ -2,39 +2,6 @@ let homeScore = document.getElementById("home")
 let guestScore = document.getElementById("guest")
 
 
-function increase(event){
-    const targetId = event.target.id
-    if(targetId === "single-home"){
-        homeScore.textContent = parseInt(homeScore.textContent) +1
-
-    }
-    if(targetId === "single-guest"){
-        guestScore.textContent= parseInt(guestScore.textContent) +1
-
-    }
-    if(targetId === "double-home"){
-        homeScore.textContent = parseInt(homeScore.textContent) +2
-
-    }
-    if(targetId === "double-guest"){
-        guestScore.textContent= parseInt(guestScore.textContent) +2
-
-    }
-    if(targetId === "triple-home"){
-        homeScore.textContent = parseInt(homeScore.textContent) +3
-
-    }
-    if(targetId === "triple-guest"){
-        guestScore.textContent= parseInt(guestScore.textContent) +3
-
-    }
-
-}
-
-function newGame(){
-    homeScore.textContent = 0
-    guestScore.textContent = 0
-}
 
 document.getElementById("single-home").addEventListener("click", () => updateScore("single-home"))
 document.getElementById("double-home").addEventListener("click", () => updateScore("double-home"))
@@ -42,6 +9,7 @@ document.getElementById("single-guest").addEventListener("click", () => updateSc
 document.getElementById("double-guest").addEventListener("click", () => updateScore("double-guest"))
 document.getElementById("triple-home").addEventListener("click", () => updateScore("triple-home"))
 document.getElementById("triple-guest").addEventListener("click", () => updateScore("triple-guest"))
+document.getElementById("new-game").addEventListener("click", newGame)
 
 function updateScore(id){
   if(id === "single-home"){
@@ -64,4 +32,8 @@ function updateScore(id){
   }
 }
 
-console.log("Hola Mundo!")
+
+function newGame(){
+    homeScore.textContent = 0
+    guestScore.textContent = 0
+}
